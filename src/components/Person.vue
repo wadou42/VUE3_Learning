e7d41586264f37b5473cf4d128691d3f6e6861ff
Hongqi Wang <3021206162@tju.edu.cn>
2023-12-28 00:05:11 +0800

    DAY1

---
+++ src/components/Person.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="Person">
+        <h2>姓名：{{name}}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click = showTel>showTel</button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Person',
+    data() {
+        return {
+            name: 'zhangsan',
+            age: 19,
+            tel:1111111
+        }
+    },
+    method: {
+        showTel() {
+            console.log(this.tel)
+        }
+    }
+}
+
+</script>
+
+<style>
+.app {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
